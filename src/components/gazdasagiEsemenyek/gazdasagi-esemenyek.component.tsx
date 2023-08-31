d91eac30a38d65d017f281eedd7f7b5e3991fcaf
beahuszar <beata.huszar@recart.com>
2023-08-31 15:09:24 +0200

    Revert "fix for deploy"
    This reverts commit 9d8d54ba27c16c2efc7f48595c82e7c8518548cf.

--- src/components/gazdasagiEsemenyek/gazdasagi-esemenyek.component.tsx
+++ src/components/gazdasagiEsemenyek/gazdasagi-esemenyek.component.tsx
@@ -8,7 +8,7 @@ export function GazdasagiEsemenyek() {
   const [text, setText] = useState('')
   useEffect(() => {
     async function fetchCsv() {
-      const readable = await fetch('/data/gazdasagi-esemenyek.csv') // todo: read into how to better handle csv in webpack, 'antd-practice/data/gazdasagi-esemenyek.csv' for development
+      const readable = await fetch('antd-practice/data/gazdasagi-esemenyek.csv') // todo: read into how to better handle csv in webpack
       const reader = readable.body?.getReader()
       const decoder = new TextDecoder('ISO-8859-2')
       const csv = await reader?.read()
