c7289106d3b453ba301a9ff1a5a761ec49829f00
beahuszar <beata.huszar@recart.com>
2023-08-31 12:16:16 +0200

    add berszamfejtes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,41 @@
 import { Collapse, Input } from 'antd'
 import { ChangeEvent, useState } from 'react'
+import { Berszamfejtes } from './components/berszamfejtes/berszamfejtes.component'
 import { Table } from './components/table'
 import { ReactComponent as ReactLogo } from './logo.svg'
 
 const text = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis purus pellentesque, feugiat magna at, consectetur leo. Vestibulum feugiat non eros sit amet feugiat. Maecenas nisl leo, blandit vel ornare et, sollicitudin ac magna. Pellentesque mollis mauris nec fringilla condimentum. Aliquam porta, nunc a ultrices rutrum, arcu nisl tristique lorem, in vestibulum ex nibh ut nisl. Cras sed pulvinar nisi. Donec elit tellus, semper ac massa ut, semper placerat nulla. Suspendisse consequat volutpat ipsum, eget lobortis dui convallis eget. Nulla non facilisis dui. Nam justo metus, ultricies id nisi iaculis, porttitor lobortis felis. Ut iaculis vitae orci eget vehicula. Pellentesque id bibendum lacus. Mauris sit amet tincidunt metus. Aliquam vehicula convallis erat quis semper. Suspendisse condimentum vehicula efficitur.
 `
-
-const defaultItems: { key: string; label: string; children: React.ReactNode }[] = [
+// TODO: valami nem oke a deployyal, csak a build foldert nyomja fel
+const defaultItems: { label: string; children: React.ReactNode }[] = [
   {
-    key: '1',
+    label: 'Bérszámfejtés',
+    children: <Berszamfejtes />
+  },
+  {
     label: 'Tablazat',
     children: <Table />
   },
   {
-    key: '2',
     label: 'Sima szöveg',
     children: <p>{text}</p>
   },
   {
-    key: '3',
     label: 'kép',
     children: <ReactLogo />
   }
 ]
 
 function App() {
-  const [items, setItems] =
-    useState<{ key: string; label: string; children: React.ReactNode }[]>(defaultItems)
+  const sortedItems = defaultItems.sort((a, b) => a.label.localeCompare(b.label))
+  const [items, setItems] = useState<{ label: string; children: React.ReactNode }[]>(sortedItems)
 
   function handleChange(event: ChangeEvent<HTMLInputElement>): void {
     const { value } = event.target
 
     if (value === '') {
-      setItems(defaultItems)
+      setItems(sortedItems)
       return
     }
 
@@ -47,7 +49,7 @@ function App() {
     <>
       Kereso:
       <Input style={{ width: 150, marginBottom: 20 }} onChange={handleChange} />
-      <Collapse items={items} defaultActiveKey={['1']} />
+      <Collapse items={items} defaultActiveKey={['0']} />
     </>
   )
 }
---
+++ src/components/berszamfejtes/berszamfejtes.component.tsx
@@ -0,0 +1,17 @@
+import bsz1 from './images/bsz1.png'
+import bsz2 from './images/bsz2.png'
+import bsz3 from './images/bsz3.png'
+import bsz4 from './images/bsz4.png'
+import bsz5 from './images/bsz5.png'
+
+export function Berszamfejtes() {
+  return (
+    <>
+      <img src={bsz1} alt='bsz1' />
+      <img src={bsz2} alt='bsz2' />
+      <img src={bsz3} alt='bsz3' />
+      <img src={bsz4} alt='bsz4' />
+      <img src={bsz5} alt='bsz5' />
+    </>
+  )
+}
